e7594fd578a1e64a5561026948ce0cc01e4f315e
Hassaan Ali <58141711+hassaanali723@users.noreply.github.com>
2021-08-12 23:54:33 +0500

    Fetch github API with Fetch

---
+++ src/Components/Searcher.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Searcher extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                Searcher
+            </div>
+         );
+    }
+}
+ 
+export default Searcher;
--- src/Github.js
+++ src/Github.js
@@ -1,14 +1,55 @@
 import React, { Component } from 'react';
+import Profile from './Components/Profile';
+import Searcher from './Components/Searcher';
+
+
+const API = 'https://api.github.com/users/';
 
 class Github extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            username: 'hassaanali723',
+            name: '',
+            avatar: '',
+            repos: '',
+            followers: '',
+            following: '',
+            homeURL: '',
+            notFound: ''
+         }
     }
+
+    getProfile(username){
+      let finalURL =  `${API}/${username}`;
+      fetch(finalURL)
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({ 
+            username: data.username,
+            name: data.name,
+            avatar: data.avatar_url,
+            repos: data.public_repos,
+            followers: data.followers,
+            following: data.following,
+            homeURL: data.html_url,
+            notFound: data.message
+         })
+    })
+    .catch((error) => console.log('There was a problem in fetching data'))
+      }
+
+    componentDidMount(){
+      this.getProfile(this.state.username)
+    }
+
     render() { 
         return ( 
         <div>
-               Welcome to Github Searcher
+               <section id="card">
+                 <Searcher />
+                 <Profile />
+               </section>
         </div> );
     }
 }
